920eb5c828830ff897a8281edf8159c17dcbf16f
k-kaki <41355856+k-kaki@users.noreply.github.com>
2020-05-06 03:55:20 +0530

    Add files via upload

---
+++ assignment1.R
@@ -0,0 +1,29 @@
+makeVector <- function(x = numeric()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmean <- function(mean) m <<- mean
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+}
+
+## it calculates the mean of the data and sets the value of the mean in the cache via the setmean function.
+
+
+cachemean <- function(x, ...) {
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m
+}
+}
